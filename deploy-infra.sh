7871362cc084b9f4e23e9178a3b028baaf08b0ca
Chris Topher <christopherbellarmine@gmail.com>
2021-08-05 13:01:15 +0530

    Add CodeDeploy/CodeBuild/CodePipeline

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -5,6 +5,14 @@ CLI_PROFILE=awsbootstrap
 EC2_INSTANCE_TYPE=t2.micro 
 AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap --query "Account" --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+
+# Generate a personal access token with repo and admin:repo_hook
+# permissions from https://github.com/settings/tokens
+GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
+GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
+GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
+GH_BRANCH=main
+
 ### Deploy the static resources
 echo -e "\n\n=========== Deploying setup.yml ==========="
 aws cloudformation deploy \
@@ -15,7 +23,26 @@ aws cloudformation deploy \
     --no-fail-on-empty-changeset \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides \
-        CodePipelineBucket = $CODEPIPELINE_BUCKET
+        CodePipelineBucket=$CODEPIPELINE_BUCKET
+
+
+### Deploy the Cloud Formation Template
+echo -e "\n\n=========== Deploying main.yml ==========="
+aws cloudformation deploy \
+    --region $REGION \
+    --profile $CLI_PROFILE \
+    --stack-name $STACK_NAME \
+    --template-file main.yml \
+    --no-fail-on-empty-changeset \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameter-overrides \
+        EC2InstanceType=$EC2_INSTANCE_TYPE \
+        GitHubOwner=$GH_OWNER \
+        GitHubRepo=$GH_REPO \
+        GitHubBranch=$GH_BRANCH \
+        GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+        CodePipelineBucket=$CODEPIPELINE_BUCKET
+
 if [ $? -eq 0 ]; then
     aws cloudformation list-exports \
         --profile awsbootstrap \
