ea602aa57f13569a60b7771f3b1e2f478d6e4970
Chris Topher <christopherbellarmine@gmail.com>
2021-08-17 11:54:01 +0530

    Add ASG

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -18,7 +18,7 @@ echo -e "\n\n=========== Deploying setup.yml ==========="
 aws cloudformation deploy \
     --region $REGION \
     --profile $CLI_PROFILE \
-    --stack-name $STACK_NAME \
+    --stack-name $STACK_NAME-setup \
     --template-file setup.yml \
     --no-fail-on-empty-changeset \
     --capabilities CAPABILITY_NAMED_IAM \
