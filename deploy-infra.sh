6b96751be09ce6a4bb58acb539846b55609c0266
Chris Topher <christopherbellarmine@gmail.com>
2021-08-18 19:21:34 +0530

    Split out staging nested stack

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -5,6 +5,7 @@ CLI_PROFILE=awsbootstrap
 EC2_INSTANCE_TYPE=t2.micro 
 AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap --query "Account" --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+CFN_BUCKET="$STACK_NAME-cfn-$AWS_ACCOUNT_ID"
 
 # Generate a personal access token with repo and admin:repo_hook
 # permissions from https://github.com/settings/tokens
@@ -13,7 +14,7 @@ GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
 GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
 GH_BRANCH=master
 
-### Deploy the static resources
+# Deploys static resources
 echo -e "\n\n=========== Deploying setup.yml ==========="
 aws cloudformation deploy \
     --region $REGION \
@@ -23,7 +24,25 @@ aws cloudformation deploy \
     --no-fail-on-empty-changeset \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides \
-        CodePipelineBucket=$CODEPIPELINE_BUCKET
+    CodePipelineBucket=$CODEPIPELINE_BUCKET \
+    CloudFormationBucket=$CFN_BUCKET
+
+# Package up CloudFormation templates into an S3 bucket
+echo -e "\n\n=========== Packaging main.yml ==========="
+mkdir -p ./cfn_output
+
+PACKAGE_ERR="$(aws cloudformation package \
+    --region $REGION \
+    --profile $CLI_PROFILE \
+    --template main.yml \
+    --s3-bucket $CFN_BUCKET \
+    --output-template-file ./cfn_output/main.yml 2>&1)"
+
+if ! [[ $PACKAGE_ERR =~ "Successfully packaged artifacts" ]]; then
+    echo "ERROR while running 'aws cloudformation package' command:"
+    echo $PACKAGE_ERR
+    exit 1
+fi
 
 
 ### Deploy the Cloud Formation Template
@@ -32,16 +51,16 @@ aws cloudformation deploy \
     --region $REGION \
     --profile $CLI_PROFILE \
     --stack-name $STACK_NAME \
-    --template-file main.yml \
+    --template-file ./cfn_output/main.yml \
     --no-fail-on-empty-changeset \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides \
-        EC2InstanceType=$EC2_INSTANCE_TYPE \
-        GitHubOwner=$GH_OWNER \
-        GitHubRepo=$GH_REPO \
-        GitHubBranch=$GH_BRANCH \
-        GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
-        CodePipelineBucket=$CODEPIPELINE_BUCKET
+    EC2InstanceType=$EC2_INSTANCE_TYPE \
+    GitHubOwner=$GH_OWNER \
+    GitHubRepo=$GH_REPO \
+    GitHubBranch=$GH_BRANCH \
+    GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 #If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
